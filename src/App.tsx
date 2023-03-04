9b1e799972076e09826ce957a92c200b522a8e1f
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-04 16:20:58 -0500

    routecontainer no longer breaks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
+
 // Components:
 import RouteContainer from "./components/RouteContainer";
 
 function App() {
   return (
     <div className="App">
+      <h1>hello world</h1>
+      <Navbar />
       <RouteContainer />
     </div>
   );
--- src/components/RouteContainer.tsx
+++ src/components/RouteContainer.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Homepage from "./Homepage";
 import Projects from "./Projects";
 
 function RouteContainer() {
   return (
-    <div>
-      <Routes>
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/" element={<Homepage />} />
+    </Routes>
   );
 }
 
