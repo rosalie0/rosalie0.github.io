761c3537cf32e410b8ed7b4718c48dd7e9c2a864
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-09 13:54:01 -0500

    got fontawesome code to import but not sure how to custom style it.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Navbar from "./components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid,
-  regular,
-  brands,
-  icon,
-} from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 // Components:
 import RouteContainer from "./components/RouteContainer";
@@ -16,8 +11,7 @@ function App() {
   return (
     <div className="App bg-rose-50" style={{ height: "100vh" }}>
       <h1 className="text-3xl text-emerald-600 font-bold ">hello world</h1>
-      <FontAwesomeIcon icon="fa-solid fa-code" />
-      <FontAwesomeIcon icon={solid("user-secret")} />
+      <FontAwesomeIcon icon={faCode} />
 
       <Navbar />
       <RouteContainer />
