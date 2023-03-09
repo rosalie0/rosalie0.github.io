f440fbc5542b6e888158adc677758aa912dcece2
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-09 13:45:08 -0500

    going to install babel in order to get fontawesome to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Navbar from "./components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+  icon,
+} from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
 // Components:
 import RouteContainer from "./components/RouteContainer";
@@ -9,6 +16,9 @@ function App() {
   return (
     <div className="App bg-rose-50" style={{ height: "100vh" }}>
       <h1 className="text-3xl text-emerald-600 font-bold ">hello world</h1>
+      <FontAwesomeIcon icon="fa-solid fa-code" />
+      <FontAwesomeIcon icon={solid("user-secret")} />
+
       <Navbar />
       <RouteContainer />
     </div>
