7fa601d2eaa16888850902944055c3f2d30e23a9
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-15 11:58:55 -0400

    proj links are buttons, added a top section to projects, wrapped projs in a section tag

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { FaCode } from "react-icons/fa";
 import { projects } from "./projectData";
 function Projects() {
   console.log(projects);
   return (
-    <div>
+    <section id="projects">
       <div>
-        {projects.map((project) => (
-          <div key={project.title} className="">
-            <h4>{project.title}</h4>
-            <a href={project.deployLink}>Deploy</a> /{" "}
-            <a href={project.githubLink}>Github</a>
-            <p>{project.description}</p>
-            <img src={project.image} alt="Screenshot previewing the web app" />
-          </div>
-        ))}
+        <div className="flex items-center">
+          <FaCode />
+          <h2>Apps I've built</h2>
+        </div>
+        <div className="container">
+          {projects.map((project) => (
+            <div key={project.title} className="">
+              <h4>{project.title}</h4>
+              <h5>{project.subtitle}</h5>
+
+              <div className="flex flex-wrap">
+                <a href={project.deployLink}>
+                  <button>Deploy</button>
+                </a>
+                <a href={project.githubLink}>
+                  <button>Github</button>
+                </a>
+              </div>
+              <p>{project.description}</p>
+              <img
+                src={project.image}
+                alt="Screenshot previewing the web app"
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
