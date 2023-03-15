b00438580c5c630be4b8576263deb6a0ba53e5d9
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-15 13:21:55 -0400

    button in navbar far right, has md light and dark icons that is the image that toggles between'

--- src/components/header/DarkModeButton.tsx
+++ src/components/header/DarkModeButton.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+
 interface ThemeProps {
   theme: string;
   /** when passing down the state setter function returned by `useState` to a child component.
@@ -12,17 +14,16 @@ const DarkModeButton = ({ theme, setTheme }: ThemeProps) => {
   };
 
   const btn =
-    "bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded";
+    "bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-2 rounded";
   const btnDark = " dark:bg-amber-200 dark:hover:bg-amber-300 dark:text-black";
   return (
-    <div>
-      {/* {theme === "light" ? <p>light mode!</p> : <p>dark mode!</p>} */}
+    <div className="pr-4">
       <button
         className={btn + btnDark}
         //className="bg-amber-600 hover:bg-amber-700 text-white dark:bg-amber-200 dark:hover:bg-amber-300 dark:text-black font-bold py-2 px-4 rounded"
         onClick={handleThemeSwitch}
       >
-        toggle
+        {theme === "dark" ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
       </button>
     </div>
   );
