f8ebc117a09efb6887313ca84066b5a3d8ce849e
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-16 01:20:52 -0400

    200 emerald cat looks better

--- src/components/header/LogoAndName.tsx
+++ src/components/header/LogoAndName.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import greenCatIcon from "../../assets/cat-green-small.png";
 import lightGreenCatIcon from "../../assets/cat-light-green-small.png";
+import light200GreenCatIcon from "../../assets/cat-200-green-small.png";
 interface Props {
   theme: string;
 }
@@ -11,7 +12,7 @@ const LogoAndName = ({ theme }: Props) => {
     <NavLink to={"/"} end>
       <div className="flex gap-2 items-center pl-4">
         <img
-          src={theme === "dark" ? lightGreenCatIcon : greenCatIcon}
+          src={theme === "dark" ? light200GreenCatIcon : greenCatIcon}
           className="h-10"
         />
         <h1 className="text-[2rem] font-comfortaa text-emerald-600 dark:text-emerald-100 font-bold">
