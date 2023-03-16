c9269830cb9bd7abbf9c19a4acc381629fcb7c2d
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-16 19:04:47 -0400

    put about back to empty p tag

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import MyCanvas from "./canvas/MyCanvas";
 
 function About() {
   return (
     <div>
       <h2>Who is 「Rose」?</h2>
-      <MyCanvas />
+      <p></p>
     </div>
   );
 }
