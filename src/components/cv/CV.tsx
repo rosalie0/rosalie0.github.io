f596403247202958d33d5f8349f70eaa087c20d1
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-15 21:27:15 -0400

    styled the page number info, flex box centers everything

--- src/components/cv/CV.tsx
+++ src/components/cv/CV.tsx
@@ -1,30 +1,33 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 import pdf from "../../assets/resume.pdf";
-//import * from "../../assets/resume.pdf"
+// If you want to use annotations (e.g. links) in PDFs rendered by React-PDF,
+// then you would need to include stylesheet necessary for annotations to be correctly displayed like so:
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// If you want to use text layer in PDFs rendered by React-PDF,
+// then you would need to include stylesheet necessary for text layer to be correctly displayed
+import "react-pdf/dist/esm/Page/TextLayer.css";
 
 // Create Document Component
 const CV = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
+  function onDocumentLoadSuccess({ numPages }: any) {
     setNumPages(numPages);
   }
 
   return (
-    <div>
-      <Document
-        //file="../../assets/resume.pdf"
-        file={pdf}
-        // file="resume.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
+    <div className="flex flex-col items-center justify-center">
+      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+
+      <div>
+        <h5 className="font-noto italic">
+          Page {pageNumber} of {numPages}
+        </h5>
+      </div>
     </div>
   );
 };
