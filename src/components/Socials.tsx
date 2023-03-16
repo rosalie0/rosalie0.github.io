122fc3562ef38a13f95250d6dfb6b7e7baff34b2
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-16 00:14:55 -0400

    dark mode styling

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,17 +3,22 @@ import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 const Socials = () => {
   return (
-    <div className="flex gap-2">
-      <a href="https://github.com/rosalie0">
-        <FiGithub />
-      </a>
-      <a href="https://www.linkedin.com/in/rosalie-newman-rn/">
-        <FiLinkedin />
-      </a>
-      <a href="mailto:rosalienewman0@gmail.com" target="_blank">
-        <FiMail />
-      </a>
-    </div>
+    <>
+      <h5 className="font-neon text-lg  text-emerald-600 dark:text-emerald-100">
+        let's connect!
+      </h5>
+      <div className="flex gap-4">
+        <a href="https://github.com/rosalie0">
+          <FiGithub className="text-emerald-600 dark:text-emerald-100 text-6xl" />
+        </a>
+        <a href="https://www.linkedin.com/in/rosalie-newman-rn/">
+          <FiLinkedin className="text-emerald-600 dark:text-emerald-100 text-6xl" />
+        </a>
+        <a href="mailto:rosalienewman0@gmail.com" target="_blank">
+          <FiMail className="text-emerald-600 dark:text-emerald-100 text-6xl" />
+        </a>
+      </div>
+    </>
   );
 };
 
