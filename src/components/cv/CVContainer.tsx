67038b1daaf75bf4223917ec9e7811844b56c9ba
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-15 21:31:03 -0400

    text when clicked opens pdf in a new page so it cna be downloaded. not styled.

--- src/components/cv/CVContainer.tsx
+++ src/components/cv/CVContainer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CV from "./CV";
+import pdf from "../../assets/resume.pdf";
 
 function CVContainer() {
   return (
-    <div>
+    <div className="container flex flex-col items-center">
       <CV />
+      <a href={pdf} target="_blank">
+        open in new window
+      </a>
     </div>
   );
 }
