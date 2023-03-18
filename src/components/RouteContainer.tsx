98128453fb9f2c58a2547adf7c8151fad247b697
Rosalie Newman <71895647+rosalie0@users.noreply.github.com>
2023-03-18 12:31:04 -0400

    Route and component cleanup (#21)
    * removed Contacts, changed order in route container
    * aboutFolder

--- src/components/RouteContainer.tsx
+++ src/components/RouteContainer.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import About from "./About";
+import About from "./about/About";
 import Art from "./Art";
-import Contact from "./Contact";
 import Error from "./Error";
 import Homepage from "./Homepage";
 import Projects from "./projects/Projects";
@@ -12,13 +11,9 @@ function RouteContainer() {
   return (
     <Routes>
       <Route path="/about" element={<About />} />
-
       <Route path="/art" element={<Art />} />
-      <Route path="/contact" element={<Contact />} />
-
-      <Route path="/projects" element={<Projects />} />
       <Route path="/cv" element={<CVContainer />} />
-
+      <Route path="/projects" element={<Projects />} />
       <Route path="/" element={<Homepage />} />
       <Route path="/*" element={<Error />} />
     </Routes>
