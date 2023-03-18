44e564931a3f907d47b4e9f2973d1d5bdf7be720
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-18 18:25:57 -0400

    got smiley to be inline using a way given in react-icons documentation to pass down things as props. added bottom padding to make it look more centered because of the font i chose has weird padding.

--- src/components/projects/MoreProjectsComingSoon.tsx
+++ src/components/projects/MoreProjectsComingSoon.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { SlEmotsmile } from "react-icons/sl";
+import { FaRegSmileWink } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const MoreProjectsComingSoon = () => {
   return (
-    <div className="text-center p-4 text-xl text-emerald-600 dark:text-emerald-50 font-frag font-bold">
-      <h5>...and more coming soon!</h5>
+    <div className="flex flex-wrap justify-center items-center gap-1 p-4 text-3xl text-emerald-600 dark:text-emerald-100 font-frag font-bold">
+      <h5>
+        ...and more coming soon!
+        <IconContext.Provider value={{ className: "inline pb-1" }}>
+          <FaRegSmileWink />
+        </IconContext.Provider>
+      </h5>
     </div>
   );
 };
