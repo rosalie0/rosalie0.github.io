ee1a805344b23690c17ceb2687f6e996312f7062
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-18 18:29:58 -0400

    gave title: desc as a value for the smiley winky to provide a desc for accessbility, had to ts ignore it.

--- src/components/projects/MoreProjectsComingSoon.tsx
+++ src/components/projects/MoreProjectsComingSoon.tsx
@@ -8,7 +8,13 @@ const MoreProjectsComingSoon = () => {
     <div className="flex flex-wrap justify-center items-center gap-1 p-4 text-3xl text-emerald-600 dark:text-emerald-100 font-frag font-bold">
       <h5>
         ...and more coming soon!
-        <IconContext.Provider value={{ className: "inline pb-1" }}>
+        <IconContext.Provider
+          value={{
+            className: "inline pb-1",
+            //@ts-ignore
+            title: "a wingling smiley face icon",
+          }}
+        >
           <FaRegSmileWink />
         </IconContext.Provider>
       </h5>
