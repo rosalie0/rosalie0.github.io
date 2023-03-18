571b735d7d46fb848904bd514d2f6484e937ea89
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-18 18:38:00 -0400

    took out title because it didnt do anything. this is a bug with react-icons that has a PR associated with it , made in october, which has not been merged.

--- src/components/projects/MoreProjectsComingSoon.tsx
+++ src/components/projects/MoreProjectsComingSoon.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { SlEmotsmile } from "react-icons/sl";
 import { FaRegSmileWink } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 const MoreProjectsComingSoon = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center gap-1 p-4 text-3xl text-emerald-600 dark:text-emerald-100 font-frag font-bold">
+    <div className="text-center p-4 text-3xl text-emerald-600 dark:text-emerald-100 font-frag font-bold">
       <h5>
         ...and more coming soon!
-        <IconContext.Provider
-          value={{
-            className: "inline pb-1",
-            //@ts-ignore
-            title: "a wingling smiley face icon",
-          }}
-        >
+        <IconContext.Provider value={{ className: "inline pb-1" }}>
           <FaRegSmileWink />
         </IconContext.Provider>
       </h5>
