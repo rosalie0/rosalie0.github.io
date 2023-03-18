066af667ae8a8479cd828f927450df7aa83cce2c
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-18 12:30:33 -0400

    fixed improper import path

--- src/components/RouteContainer.tsx
+++ src/components/RouteContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import About from "./About";
+import About from "./about/About";
 import Art from "./Art";
 import Error from "./Error";
 import Homepage from "./Homepage";
