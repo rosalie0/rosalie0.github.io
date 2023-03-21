bc1e67e54f22fad1b53a71a53a802f3e53c9ffc5
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-20 21:57:30 -0400

    trying to make own environ but console breaks. asking discord for help

--- src/components/canvas/StageForCat.tsx
+++ src/components/canvas/StageForCat.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Stage } from "@react-three/drei";
 import Cat from "./Cat";
 import { Environment } from "@react-three/drei";
+import studio from "../../assets/studio_small_03_1k_LOWRES.png?url";
 
 const StageForCat = () => {
   return (
     <Stage
-      environment={"studio"}
+      // environment={{ files: studio }}
+      //environment={{ files: "/studio_small_03_1k_LOWRES.png" }}
+      //environment={undefined}
+      environment={undefined}
       adjustCamera={true}
-      shadows={{ type: "contact", offset: 1 }}
+      // shadows={{ type: "contact", offset: 1 }}
     >
-      <Cat />
+      {/* <Cat /> */}
     </Stage>
   );
 };
