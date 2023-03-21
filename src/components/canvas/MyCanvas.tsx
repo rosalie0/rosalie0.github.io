9b876153ad465a0c105a9ba62bf6a462bc7ad325
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-20 21:18:13 -0400

    removed backdrop because of its weird loading issues depending on screen size. need to make a custom environment so as to avoid the 1.5mb load of the image

--- src/components/canvas/MyCanvas.tsx
+++ src/components/canvas/MyCanvas.tsx
@@ -7,15 +7,20 @@ import PinkBackdrop from "./PinkBackdrop";
 function MyCanvas() {
   return (
     <div style={{ height: "95vh", width: "100%", backgroundColor: "pink" }}>
-      <Canvas camera={{ position: [7.5, -1, 0], fov: 75 }}>
+      <Canvas
+        resize={{ scroll: false }}
+        camera={{ position: [7.5, -1, 0], fov: 75 }}
+      >
         <Suspense fallback={null}>
-          <Stage adjustCamera={true} shadows={{ type: "contact", offset: 1 }}>
-            <PresentationControls>
-              <Cat />
-            </PresentationControls>
+          <Stage
+            // environment={"studio"}
+            adjustCamera={true}
+            shadows={{ type: "contact", offset: 1 }}
+          >
+            <Cat />
           </Stage>
 
-          <PinkBackdrop />
+          {/* <PinkBackdrop /> */}
           <pointLight color={"red"} intensity={2} position={[3, 3, 3]} />
         </Suspense>
       </Canvas>
