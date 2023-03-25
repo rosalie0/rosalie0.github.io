cac8f6fc249b31537c6250a096e2f9f80e37d6e9
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-25 12:14:35 -0400

    moved about pic to public folder so i can preload it and it helped performance and lcp so much!

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -11,9 +11,11 @@ function About() {
       className="flex flex-col gap-4 items-center lg:flex-row container p-4 lg:p-20"
     >
       <img
-        src={portrait}
+        alt="an anime drawing of a girl with long brown hair and glasses, smiling with confidence"
+        src="/portrait-square-95.webp"
+        //src={portrait}
         className="rounded-full md:float-left max-h-100% h-auto"
-        // todo: give ti height/width value to reduce how content moves around on slow loading
+        // todo: give t height/width value to reduce CLS
         // but need to have it reflect with responsive design
         // height={"600px"}
         // width={"600px"}
