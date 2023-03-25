e3efb6240a453246fb7513b4b1a7143311d08820
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-25 12:32:51 -0400

    some dark mode color adjusts

--- src/components/about/AboutHeading.tsx
+++ src/components/about/AboutHeading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutHeading = () => {
   return (
-    <h2 className="text-center leading-tight lg:text-left font-frag font-bold text-6xl text-rose-900">
+    <h2 className="text-center leading-tight lg:text-left font-frag font-bold text-6xl text-rose-900 dark:text-rose-200">
       Hi! I'm 「Rosalie」
     </h2>
   );
