0c87f4877954b60ceeb9b61a49bf4922b9b16853
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-05 17:23:20 -0400

    added techStack to Project class. array of strings

--- src/components/projects/projectData.ts
+++ src/components/projects/projectData.ts
@@ -9,6 +9,7 @@ import bugHuntImageTiny from "../../assets/projects/webp/bug-hunt-preview700.web
 class Project {
   title!: string;
   subtitle?: string;
+  techStack!: string[];
   description!: string;
   image!: string;
   deployLink?: string;
@@ -21,41 +22,44 @@ class Project {
 }
 
 const capstone = new Project({
-  deployLink: "https://codenames-0nt7.onrender.com/",
-  githubLink: "https://github.com/2208-Capstone-Team-2/codenames",
-  videoLink: "https://www.youtube.com/watch?v=nMLCx3XAIkE",
   title: "Codenames",
   subtitle: "an online multiplayer card game",
+  techStack: [""],
   description: ` Written in typescript with a blend of firebase & SQL for backend.
     Features synchronized gameplay across all players' screens, responsive design, and user chat box for a seamless user experience. 
     Created by a team of five members, over the course of 6 weeks.
     `,
   image: codenamesImageTiny,
+  deployLink: "https://codenames-0nt7.onrender.com/",
+  githubLink: "https://github.com/2208-Capstone-Team-2/codenames",
+  videoLink: "https://www.youtube.com/watch?v=nMLCx3XAIkE",
 });
 
 const graceShopper = new Project({
-  deployLink: "https://comic-overflow.onrender.com/",
-  githubLink: "https://github.com/2208-GS-Team-1/Comic-Overflow",
-  videoLink: "https://www.youtube.com/watch?v=rbbKldsF4Gw",
   title: "Comic Overflow",
   subtitle: "an e-commerce site",
+  techStack: [""],
   description: `   Comic Overflow is an e-commerce platform for comic book enthusiasts built over the course of a 2.5 week period in a team of four.
     Features include a from-scratch backend schema, account creation & authentication, persistent user cart, Stripe API checkout, and admin view that lets website owners add and delete products and orders from the webpage.
     Backend schemas designed and written from scratch, with Express API routes protected with JWT authentication.
     `,
   image: graceShopperImageTiny,
+  deployLink: "https://comic-overflow.onrender.com/",
+  githubLink: "https://github.com/2208-GS-Team-1/Comic-Overflow",
+  videoLink: "https://www.youtube.com/watch?v=rbbKldsF4Gw",
 });
 
 const stackathon = new Project({
-  deployLink: "https://bug-hunt.onrender.com/",
-  githubLink: "https://github.com/rosalie0/Bug-Hunt",
   title: "Bug Hunt",
   subtitle: "a singleplayer 3D web game",
+  techStack: [""],
   description: `    A solo foray into 3-D rendering on the web, this game has players collect points by clicking randomly generated insect models, with a timer feature and leaderboard where they can submit their score.
     React Three Fiber and Three.js were used to display ported 3D blender models, create 3D text, and change 2D photographs into skyboxes.
     Redux managed state and Firebase Cloud Firestore was used for the leaderboard backend.
     `,
   image: bugHuntImageTiny,
+  deployLink: "https://bug-hunt.onrender.com/",
+  githubLink: "https://github.com/rosalie0/Bug-Hunt",
 });
 
 const projects = [capstone, graceShopper, stackathon];
