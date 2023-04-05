5542ff78bbc3a5d3bbcc14a3901f1f152d860b0e
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-05 19:07:50 -0400

    adjusted proj text for better spacing

--- src/components/projects/projectData.ts
+++ src/components/projects/projectData.ts
@@ -33,10 +33,10 @@ const capstone = new Project({
     "Express",
     "Sequelize",
   ],
-  description: ` Online version of the popular card game, Codenames.
-    Features synchronized gameplay across all players' screens, responsive design, and user chat box for an engaging user experience. Backend & game logic written from scratch
-    Created by a team of five members, over the course of 6 weeks.
-    `,
+  description: `  Online version of the popular card game, Codenames.
+Features synchronized gameplay across all players' screens, responsive design, and user chat box for an engaging user experience.
+  Challenges included seeding random board layouts, and managing complex game logic and sockets.
+  Created by a team of five members over six weeks.`,
   image: codenamesImageTiny,
   deployLink: "https://codenames-0nt7.onrender.com/",
   githubLink: "https://github.com/2208-Capstone-Team-2/codenames",
@@ -48,10 +48,9 @@ const graceShopper = new Project({
   title: "Comic Overflow",
   subtitle: "an e-commerce site",
   techStack: ["Javascript", "React", "Redux", "Express", "Sequelize"],
-  description: `   Comic Overflow is an e-commerce platform for comic book enthusiasts built over the course of a 2.5 week period in a team of four.
-    Features include user account creation & authentication, a persistent cart for both logged-in and guest users, Stripe API checkout. Users with 'admin' role are able to add, edit and delete products, orders and even other users all from the browser.
-    API routes protected with JWT authentication.
-    `,
+  description: `  Comic Overflow is an e-commerce platform for comic book enthusiasts built over the course of a 2.5 week period in a team of four.
+  Features include user account creation & authentication, a persistent cart for both logged-in and guest users, Stripe API checkout. Users with 'admin' role are able to add, edit and delete products, orders and even other users all from the browser.
+  API routes protected with JWT authentication.`,
   image: graceShopperImageTiny,
   deployLink: "https://comic-overflow.onrender.com/",
   githubLink: "https://github.com/2208-GS-Team-1/Comic-Overflow",
@@ -69,10 +68,9 @@ const stackathon = new Project({
     "React Three Fiber",
     "Firebase",
   ],
-  description: `    A solo foray into 3-D rendering on the web, this game has players collect points by clicking randomly generated insect models, with a timer feature and leaderboard where they can submit their score.
-    React Three Fiber and Three.js were used to display ported 3D blender models, create 3D text, and change 2D photographs into skyboxes.
-    Redux managed state and Firebase Cloud Firestore was used for the leaderboard backend.
-    `,
+  description: `  A solo foray into 3-D rendering on the web, this game has players collect points by clicking randomly generated insect models, with a timer feature and leaderboard where they can submit their score.
+    Features ported 3D blender models, font-based 3D text, and 2D photographs made into skyboxes.
+    Firebase Cloud Firestore was used for the leaderboard backend.`,
   image: bugHuntImageTiny,
   deployLink: "https://bug-hunt.onrender.com/",
   githubLink: "https://github.com/rosalie0/Bug-Hunt",
