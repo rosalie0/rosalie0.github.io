44e9f5503d445cf9692f6ed44875a3cb5dd2a245
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-10 16:02:31 -0400

    dark green and white cat in header instead

--- src/components/header/LogoAndName.tsx
+++ src/components/header/LogoAndName.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import greenCatIcon from "../../assets/catIcon/cat-green-small.png";
-import light200GreenCatIcon from "../../assets/catIcon/cat-200-green-small.png";
+import greenCat from "../../assets/catIcon/cat-emerald-900.png";
+import whiteCat from "../../assets/catIcon/cat-white.png";
 import { animateScroll as scroll } from "react-scroll";
 
 interface Props {
@@ -17,7 +17,7 @@ const LogoAndName = ({ theme }: Props) => {
       <div className="flex gap-2 items-center pl-4">
         <img
           alt="a green cartoon cat face smiling"
-          src={theme === "dark" ? light200GreenCatIcon : greenCatIcon}
+          src={theme === "dark" ? whiteCat : greenCat}
           className="h-10"
         />
         <h1 className="text-[2rem] whitespace-nowrap font-comfortaa text-emerald-900 dark:text-emerald-100 font-bold">
