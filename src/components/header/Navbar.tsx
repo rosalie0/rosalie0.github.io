80a316e2ae7a570865169aa6a54e6f27d062af8d
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-15 14:04:48 -0400

    navbar removed commented out / unused state

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -3,10 +3,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
-interface Props {
-  offsetHeight: number;
-}
-const Navbar = ({ offsetHeight }: Props) => {
+
+const Navbar = ({ offsetHeight }: { offsetHeight: number }) => {
   // A piece of state that uses an eventListener to see if we are at scrollY of 0
   const [userAtTop, setUserAtTop] = useState(true);
   useScrollPosition(({ prevPos, currPos }) => {
@@ -27,24 +25,12 @@ const Navbar = ({ offsetHeight }: Props) => {
     "font-frag text-emerald-900 dark:text-emerald-200 cursor-pointer";
   const hoverStyles = "hover:text-amber-600 dark:hover:text-amber-200";
 
-  const [aboutIsActive, setAboutIsActive] = useState(false);
-  const [projectsIsActive, setProjectsIsActive] = useState(false);
-  const [contactIsActive, setContactIsActive] = useState(false);
-
   return (
     <div className={notActiveLinkStyles} style={navbarStyle}>
       <Link
         to="about"
         className={userAtTop ? activeLinkStyles + hoverStyles : hoverStyles}
         activeClass={activeLinkStyles}
-        // className={aboutIsActive ? activeLinkStyles : notActiveLinkStyles}
-        // onClick={() => {
-        //   setContactIsActive(false);
-        //   setAboutIsActive(true);
-        //   setProjectsIsActive(false);
-        // }}
-        // onSetActive={() => setAboutIsActive(true)}
-        // onSetInactive={() => setAboutIsActive(false)}
         smooth={true}
         offset={offsetHeight}
         isDynamic={true}
@@ -57,14 +43,6 @@ const Navbar = ({ offsetHeight }: Props) => {
         to="projects"
         className={hoverStyles}
         activeClass={activeLinkStyles}
-        // className={projectsIsActive ? activeLinkStyles : notActiveLinkStyles}
-        // onClick={() => {
-        //   setContactIsActive(false);
-        //   setAboutIsActive(false);
-        //   setProjectsIsActive(true);
-        // }}
-        // onSetActive={() => setProjectsIsActive(true)}
-        // onSetInactive={() => setProjectsIsActive(false)}
         smooth={true}
         offset={offsetHeight}
         isDynamic={true}
@@ -77,14 +55,6 @@ const Navbar = ({ offsetHeight }: Props) => {
         to="contact"
         className={hoverStyles}
         activeClass={activeLinkStyles}
-        // className={contactIsActive ? activeLinkStyles : notActiveLinkStyles}
-        // onClick={() => {
-        //   setContactIsActive(true);
-        //   setAboutIsActive(false);
-        //   setProjectsIsActive(false);
-        // }}
-        // onSetActive={() => setContactIsActive(true)}
-        // onSetInactive={() => setContactIsActive(false)}
         smooth={true}
         offset={offsetHeight}
         isDynamic={true}
